7bd9aa4c3ad9c31ef2df366fd8c426c389abd620
Anil Gencdogus <agencdogus@ziyotek.net>
2023-11-07 05:25:37 +0300

    added remote module

--- modules.tf
+++ modules.tf
@@ -1,5 +1,7 @@
 module "security_groups" {
-  source          = "../modules/security_groups"
+  source  = "app.terraform.io/024_2023-summer-cloud/security-groups/aws"
+  version = "1.0.0"
+  vpc_id = aws_vpc.main.id
   security_groups = var.security_groups
-  vpc_id          = aws_vpc.main.id
+  # insert required variables here
 }
